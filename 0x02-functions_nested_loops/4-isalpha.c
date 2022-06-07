828fa25fb93ecc8646f75bb58442c62457aca96d
KateM27 <mamocatherine@gmail.com>
2022-06-07 06:11:07 -0700

    check lower_upper

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - main function
+ *
+ * @c: chaaracter to compare
+ *
+ * Return: 1 if true, 0 if otherwise
+ *
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		{
+			return (1);
+		}
+	return (0);
+}
