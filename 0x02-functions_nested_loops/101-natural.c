4581f4526b954db0285e1585c3e022f99778efc8
KateM27 <mamocatherine@gmail.com>
2022-06-08 04:46:34 -0700

    natural numbers < 1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+	int n;
+	int sum = 0;
+
+	for (n = 0; n < 1024; n++)
+	{
+		if ((n % 3) == 0 || (n % 5) == 0)
+			sum += n;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
