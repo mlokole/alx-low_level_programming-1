d0668f07bc964a069a03c65e30f9377c0446bfef
KateM27 <mamocatherine@gmail.com>
2022-06-09 08:42:37 -0700

    calendar 366

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -6,7 +6,7 @@
  * @month: month in number format
  * @day: day of month
  * @year: year
- * Return: void
+ * Return: date considering leap year
  */
 
 void print_remaining_days(int month, int day, int year)
