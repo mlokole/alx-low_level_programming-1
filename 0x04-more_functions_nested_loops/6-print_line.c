87dc5bff8edae080b9b8374b3443b65dbe719b23
KateM27 <mamocatherine@gmail.com>
2022-06-09 06:31:06 -0700

    line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -4,16 +4,17 @@
  * print_line - function that prints a line
  *
  * @n: number of times the character _ should be printed
+ *
+ * Return: a new line if n is 0 or less, otherwise a straight line
  */
 
 void print_line(int n)
 {
 	int l;
 	
-	if (n > 0)
+	for (l = 0; l < n; l++)
 	{
-		for (l = 0; l < n; l++)
-			_putchar('_');
+		_putchar('_');
 	}
 	_putchar('\n');
 }
