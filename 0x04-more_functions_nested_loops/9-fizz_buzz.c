209239ebf605388e323db42d18ce226ce0f67574
KateM27 <mamocatherine@gmail.com>
2022-06-09 07:39:05 -0700

    fuzz-bizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,28 +13,23 @@ int main(void)
 
 	for (a = 1; a <= 100; a++)
 	{
-		if (a % 3 == 0 && a % 5 == 0)
-		{
+		if ((a % 3) == 0 && (a % 5) == 0)
 			printf("FizzBuzz");
-		}
-		else if (a % 3 == 0)
-		{
+
+		else if ((a % 3) == 0)
 			printf("Fizz");
-		}
-		else if (a % 5)
-		{
+
+		else if ((a % 5) == 0)
 			printf("Buzz");
-		}
+
 		else
-		{
 			printf("%d", a);
-		}
+
 		if (a == 100)
-		{
 			continue;
-		}
 		printf(" ");
 	}
+
 	printf("\n");
 
 	return (0);
