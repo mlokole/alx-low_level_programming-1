d1717a27daed0938e01e186f3465412c92de23a3
KateM27 <mamocatherine@gmail.com>
2022-06-10 02:16:06 -0700

    calendar correction

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -2,7 +2,8 @@
 #include "main.h"
 
 /**
- * print_remaining_days - takes a date and prints how many days are left in the year, taking leap years into account
+ * print_remaining_days - takes a date and prints how many days are 
+ * left in the year,taking leap years into account
  * @month: month in number format
  * @day: day of month
  * @year: year
@@ -15,7 +16,7 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (month >= 3 && day >= 60)
 		{
-			day ++;
+			day++;
 		}
 
 		printf("Day of the year: %d\n", day);
