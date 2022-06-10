f5278e7f00befeb072939f0d646d44cf4f008970
KateM27 <mamocatherine@gmail.com>
2022-06-10 03:00:30 -0700

    cal correction

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,9 +14,11 @@ void print_remaining_days(int month, int day, int year)
 {
 	if (year % 4 == 0 || year % 400 == 0)
 	{
-	if (month >= 3 && day >= 60)
-	{
-		day++;
+		if (month >= 3 && day >= 60)
+		{
+			day++;
+		}
+		
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
@@ -31,7 +33,6 @@ void print_remaining_days(int month, int day, int year)
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
 		}
-	}
 	}
 }
 
