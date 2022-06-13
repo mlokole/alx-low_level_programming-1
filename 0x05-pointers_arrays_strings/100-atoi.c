0c013714ba947f82248f78d58f69a64109146b26
KateM27 <mamocatherine@gmail.com>
2022-06-13 15:20:58 -0700

    str-int

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include "2-strlen.c"
+/**
+ * _atoi - function converts a string to an integer
+ *
+ * @s: string to convert
+ * Return: 0 if there are no numbers in the string
+ */
+int _atoi(char *s)
+{
+	int n;
+	int con = 0;
+	int i;
+	int j = 1;
+	int num = 0;
+
+	for (n = 0; n < _strlen(s); n++)
+	{
+		if (!(s[n] >= '0' && s[n] <= '9') && n > 0)
+		{
+			break;
+		}
+		if (s[n] == '-')
+			con--;
+		if (s[n] == '+')
+			con++;
+		if (s[n] >= '0' && s[n] <= '9')
+			i++;
+	}
+	while (i > 0)
+	{
+		num += ((s[n - 1] - '0') * j);
+		n--;
+		i--;
+		j *= 10;
+	}
+	if (con >= 0)
+		num *= 1;
+	else
+		num *= -1;
+	return (num);
+}
