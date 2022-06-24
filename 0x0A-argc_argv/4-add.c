4caf10f5a42c1070d1c6097bde7b735143de6e71
KateM27 <mamocatherine@gmail.com>
2022-06-24 10:25:55 -0700

    addition

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-#include <stlib.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 /**
  * main - Entry point
@@ -10,4 +11,21 @@
  */
 int main(int argc, char *argv[])
 {
+	int x, y;
+	int sum = 0;
+
+	for (x = 1; x < argc; x++)
+	{
+		for (y = 0; argv[x][y] != '\0'; y++)
+		{
+			if (!isdigit(argv[x][y]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[x]);
+	}
+	printf("%d\n", sum);
+	return (0);
 }
