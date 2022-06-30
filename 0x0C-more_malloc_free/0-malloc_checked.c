54f494dcc9d80f616f468221a5bc8685bab38c1f
KateM27 <mamocatherine@gmail.com>
2022-06-30 04:11:33 -0700

    mem allocation

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - function allocates memory
+ * @b: allocated bytes
+ *
+ * Return: pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *mem;
+
+	mem = malloc(b);
+
+	if (mem == NULL)
+		exit(98);
+	return (mem);
+}
