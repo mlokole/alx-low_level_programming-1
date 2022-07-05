28811f8932b804bccbdc11653225770376c1aa73
KateM27 <mamocatherine@gmail.com>
2022-07-05 02:05:57 -0700

    fixed error

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,6 +9,8 @@
  */
 void print_dog(struct dog *d)
 {
+	if (d == NULL)
+		return;
 	if (d->name == NULL)
 		printf("Name: (nil)\n");
 	else
@@ -21,6 +23,4 @@ void print_dog(struct dog *d)
 		printf("Owner: (nil)\n");
 	else
 		printf("Owner: %s\n", d->owner);
-	if (d == NULL)
-		return;
 }
